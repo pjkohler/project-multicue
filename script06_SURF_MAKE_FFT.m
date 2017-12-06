583a2a6aa26589688c64181e785b8abed5529d07
pjkohler <pjkohl3r@gmail.com>
2017-12-05 17:31:18 -0800

    made compatible with experiment 3

--- script06_SURF_MAKE_FFT.m
+++ script06_SURF_MAKE_FFT.m
@@ -1,68 +1,60 @@
 clear all
 close all
+setenv('DYLD_LIBRARY_PATH','')
 
-topDir = '/Volumes/Denali_MRI/kohler/fMRI_EXP/MULTIFOVEA';
-suffix = 'vr.sc.dt';
 doSmooth = true;
+experiment = 3;
 
-if doSmooth
-    surfSuffix = [suffix,'.3fwhm'];
-else
-    surfSuffix = suffix;
+switch experiment
+    case 1
+        topDir = '/Volumes/Denali_MRI/kohler/fMRI_EXP/MULTIFOVEA';
+        conditions = {'cont','mofo','disp'};
+        suffix = 'vr.sc.dt';
+        if doSmooth
+            surfSuffix = [suffix,'.3fwhm'];
+        else
+            surfSuffix = suffix;
+        end
+    case 3
+        topDir = '/Volumes/Denali_MRI/kohler/fMRI_EXP/MULTIFOVEA_ATT';
+        conditions = {'motAtt','dispAtt'};
+        suffix = 'vr.sc.dt_al';
+        if doSmooth
+            surfSuffix = [suffix,'_3fwhm'];
+        else
+            surfSuffix = suffix;
+        end
+    otherwise
+        msg = sprintf('\nunknown experiment %0.0d\n',experiment);
+        error(mgs);
 end
+
 
 subjFolders = subfolders([topDir,'/201*'],1);
 for s =1:length(subjFolders);
-    CONTinput = subfiles([subjFolders{s},'/run*cont*',suffix,'.nii.gz'],1);
-    MOFOinput = subfiles([subjFolders{s},'/run*mofo*',suffix,'.nii.gz'],1);
-    DISPinput = subfiles([subjFolders{s},'/run*disp*',suffix,'.nii.gz'],1);
-    outSurf = [subjFolders{s},'/SURF'];
-    runSurfDir = [subjFolders{s},'/SURF/run_surf'];
-    if ~exist(outSurf,'dir')
-        mkdir(outSurf);
-    elseif ~exist(runSurfDir,'dir')
-        mkdir(runSurfDir);
-    end
-    CONToutput = [outSurf,'/CONT_signal.nii.gz'];
-    MOFOoutput = [outSurf,'/MOFO_signal.nii.gz'];
-    DISPoutput = [outSurf,'/DISP_signal.nii.gz'];
-    if ~exist(CONToutput,'file')
-        VOL_FFT(CONTinput,CONToutput,10);
-    else
-    end
-    if ~exist(MOFOoutput,'file')
-        VOL_FFT(MOFOinput,MOFOoutput,10);
-    else
-    end
-    if DISPinput{1}(1)~=0 && ~exist(DISPoutput,'file')
-           VOL_FFT(DISPinput,DISPoutput,10);
-    else
-    end
-%     
-%     topSurf = subfiles(sprintf('%s/*.niml.dset*',subjFolders{s}),1);
-%     if topSurf{1}~=0 % % if surf files in the top level folder, move to SURF folder
-%         for t=1:length(topSurf);
-%             movefile(topSurf{t},[runSurfDir,'/.']);
-%         end
-%     else
-%     end
-    hemi = {'lh','rh'};
-    for h = 1:length(hemi)
-        if ~exist(sprintf('%s/%s.std.141.CONT_signal_%s.runSmooth.niml.dset',outSurf,hemi{h},surfSuffix),'file') && ...
-           ~exist(sprintf('%s/%s.std.141.MOFO_signal_%s.runSmooth.niml.dset',outSurf,hemi{h},surfSuffix),'file');
-                CONTsurfinput = subfiles(sprintf('%s/%s.std.141.run*cont*%s.niml.dset',runSurfDir,hemi{h},surfSuffix),1);
-                MOFOsurfinput = subfiles(sprintf('%s/%s.std.141.run*mofo*%s.niml.dset',runSurfDir,hemi{h},surfSuffix),1);
-                CONTsurfoutput = sprintf('%s/%s.std.141.CONT_signal_%s.runSmooth.niml.dset',outSurf,hemi{h},surfSuffix);
-                MOFOsurfoutput = sprintf('%s/%s.std.141.MOFO_signal_%s.runSmooth.niml.dset',outSurf,hemi{h},surfSuffix);
-                SURF_FFT(CONTsurfinput,CONTsurfoutput,10);
-                SURF_FFT(MOFOsurfinput,MOFOsurfoutput,10);
+    for c = 1:length(conditions)
+        volInput = subfiles(...
+            sprintf('%s/run*%s*%s.nii.gz',subjFolders{s},conditions{c},suffix),1);
+        outSurf = [subjFolders{s},'/SURF'];
+        volOutput = sprintf('%s/%s_signal.nii.gz',outSurf,conditions{c});
+        if volInput{1}(1)~=0 && ~exist(volOutput,'file')
+            mriBrainFFT(volInput,volOutput,10);
         else
         end
-        DISPsurfinput = subfiles(sprintf('%s/%s.std.141.run*disp*%s.niml.dset',runSurfDir,hemi{h},surfSuffix),1);
-        if DISPsurfinput{1}(1)~=0 && ~exist(sprintf('%s/%s.std.141.DISP_signal_%s.runSmooth.niml.dset',outSurf,hemi{h},surfSuffix),'file')
-            DISPsurfoutput = sprintf('%s/%s.std.141.DISP_signal_%s.runSmooth.niml.dset',outSurf,hemi{h},surfSuffix);
-            SURF_FFT(DISPsurfinput,DISPsurfoutput,10);
-        else
+        runSurfDir = [subjFolders{s},'/SURF/run_surf'];
+        if ~exist(outSurf,'dir')
+            mkdir(outSurf);
+        elseif ~exist(runSurfDir,'dir')
+            mkdir(runSurfDir);
+        end
+        hemi = {'lh','rh'};
+        for h = 1:length(hemi)
+            surfInput = subfiles(sprintf('%s/%s.std.141.run*%s*%s.niml.dset',runSurfDir,hemi{h},conditions{c},surfSuffix),1);
+            surfOutput = sprintf('%s/%s.std.141.%s_signal_%s.runSmooth.niml.dset',outSurf,hemi{h},conditions{c},surfSuffix);
+            if surfInput{1}(1)~=0 && ~exist(surfOutput,'file')
+                mriBrainFFT(surfInput,surfOutput,10);
+            else
+            end
         end
     end
 end
